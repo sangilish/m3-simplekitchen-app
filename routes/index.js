8db1465ad9719aa7a41123b1793781a9611da8ff
sangilish <56685007+sangilish@users.noreply.github.com>
2024-10-14 13:23:15 -0700

    w6-d3-homework

---
+++ routes/index.js
@@ -0,0 +1,66 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const path = require('path');
+const auth = require('http-auth');
+const { check, validationResult } = require('express-validator');
+
+const router = express.Router();
+const Registration = mongoose.model('Registration');
+const basic = auth.basic({
+  file: path.join(__dirname, '../users.htpasswd'),
+});
+
+router.get('/', (req, res) => {
+  //res.send('It works!');
+  res.render('home', { title: 'Home Page' });
+});
+
+router.get('/register', (req, res) => {
+  //res.send('It works!');
+  res.render('form', { title: 'Registration form' });
+});
+
+router.get('/registrants', basic.check((req, res) => {
+  Registration.find()
+    .then((registrations) => {
+      res.render('registrants', { title: 'Listing registrations', registrations });
+    })
+    .catch(() => { 
+      res.send('Sorry! Something went wrong.'); 
+    });
+}));
+
+router.post('/', 
+    [
+        check('name')
+        .isLength({ min: 1 })
+        .withMessage('Please enter a name'),
+        check('email')
+        .isLength({ min: 1 })
+        .withMessage('Please enter an email'),
+    ],
+    async (req, res) => {
+        //console.log(req.body);
+        const errors = validationResult(req);
+        if (errors.isEmpty()) {
+          const registration = new Registration(req.body);
+          // generate salt to has password
+          const salt = await bcrypt.genSalt(10);
+          // set user password to hased password
+          registration.password = await brcypt.hash(registration.password, salt);
+          registration.save()
+            .then(() => {res.render('thankyou', { title: 'Thank you for Registration' });})
+            .catch((err) => {
+              console.log(err);
+              res.send('Sorry! Something went wrong.');
+            });
+          } else {
+            res.render('form', { 
+                title: 'Registration form',
+                errors: errors.array(),
+                data: req.body,
+             });
+          }
+    });
+
+module.exports = router;
